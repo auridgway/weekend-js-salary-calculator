8c1f412292f0bb88dfbe0934a4f10aee5a405746
Gus Ridgway <auridgway@gmail.com>
2024-01-05 16:54:34 -0600

    added all functionality

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 let employees = [];
+let employeeCounter = 0;
+let totalMonthlyCost = 0.00;
+
 const globalInputs = {firstName: document.getElementById('firstNameInput'), lastName: document.getElementById('lastNameInput'), empID: document.getElementById('inputForID'), title: document.getElementById('titleInput'), annualSalary: document.getElementById('annualSalaryInput')}
 const addEmployeeToArray = (firstName, lastName, empID, title, annualSalary) => employees.push({firstName, lastName, empID, title, annualSalary});
 
-const removeRow = (e) => e.target.closest('tr').remove();
+const removeRow = (e) => {
+    totalMonthlyCost -= employees[parseInt(e.target.closest('tr').id)].annualSalary;
+    document.getElementById("monthlyBudget").innerText = new Intl.NumberFormat('en-US', {style: 'currency',currency: 'USD'}).format(totalMonthlyCost);
+    employees.splice(0, parseInt(e.target.closest('tr').id));
+    employeeCounter = employees.length;
+
+    e.target.closest('tr').remove();
+}
 
 function submitForm(event){
     event.preventDefault();
-    let totalMonthlyCost = 0.00;
+
+    employeeCounter = employees.length;
     addEmployeeToArray(globalInputs.firstName.value, globalInputs.lastName.value, globalInputs.empID.value, globalInputs.title.value, parseFloat(globalInputs.annualSalary.value));
 
     document.getElementById('tableBody').innerHTML +=`
-    <tr>
+    <tr id="${employeeCounter}">
         <td>${globalInputs.firstName.value}</td>
         <td>${globalInputs.lastName.value}</td>
         <td>${globalInputs.empID.value}</td>
@@ -27,4 +38,3 @@ function submitForm(event){
         document.getElementById("monthlyBudget").style.backgroundColor = 'red';
     }
 }
-
