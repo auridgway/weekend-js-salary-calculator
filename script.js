7e755a9b3111aa1a9f012291b6fe9dfa4152cd9b
Gus Ridgway <auridgway@gmail.com>
2024-01-05 16:37:14 -0600

    added dom manipulation as well as the delete button on table with some formatting changes

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-
 let employees = [];
 const globalInputs = {firstName: document.getElementById('firstNameInput'), lastName: document.getElementById('lastNameInput'), empID: document.getElementById('inputForID'), title: document.getElementById('titleInput'), annualSalary: document.getElementById('annualSalaryInput')}
-const createEmployee = (firstName, lastName, empID, title, annualSalary) => employees.push({firstName, lastName, empID, title, annualSalary});
+const addEmployeeToArray = (firstName, lastName, empID, title, annualSalary) => employees.push({firstName, lastName, empID, title, annualSalary});
 
+const removeRow = (e) => e.target.closest('tr').remove();
 
 function submitForm(event){
     event.preventDefault();
+    let totalMonthlyCost = 0.00;
+    addEmployeeToArray(globalInputs.firstName.value, globalInputs.lastName.value, globalInputs.empID.value, globalInputs.title.value, parseFloat(globalInputs.annualSalary.value));
 
-    createEmployee(globalInputs.firstName.value, globalInputs.lastName.value, globalInputs.empID.value, globalInputs.title.value, globalInputs.annualSalary.value)
+    document.getElementById('tableBody').innerHTML +=`
+    <tr>
+        <td>${globalInputs.firstName.value}</td>
+        <td>${globalInputs.lastName.value}</td>
+        <td>${globalInputs.empID.value}</td>
+        <td>${globalInputs.title.value}</td>
+        <td>${new Intl.NumberFormat('en-US', {style: 'currency',currency: 'USD'}).format(globalInputs.annualSalary.value)}</td>
+        <td><button onClick="removeRow(event)">❌</button></td>
+    </tr>`
+
+    for (const person of employees){
+        totalMonthlyCost += person.annualSalary;
+    }
+    document.getElementById("monthlyBudget").innerText = new Intl.NumberFormat('en-US', {style: 'currency',currency: 'USD'}).format(totalMonthlyCost);
+    if (totalMonthlyCost > 20000){
+        document.getElementById("monthlyBudget").style.backgroundColor = 'red';
+    }
 }
 
